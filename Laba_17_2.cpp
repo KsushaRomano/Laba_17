7ca5ac2ebbf3e2f8eca703dd5a8c783c9400b819
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-18 11:53:03 +0300

    Add files via upload

---
+++ Laba_17_2.cpp
@@ -0,0 +1,76 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N, i, h, l, k, j, p, L, g;
+	k = 0;
+	l = 1;
+	p = 0;
+	j = 1;
+	g = 0;
+	cout << "Введите размер массива: ";
+	cin >> N;
+	int* A = new int[N];
+	cout << "Введите элементы массива: " << endl;
+	for (i = 0; i < N; i++) {
+		cin >> A[i];
+	};
+	cout << "Введите размер серии L: ";
+	cin >> L;
+	h = A[0];
+	for (i = 1; i <= N; i++) {
+		if (h != A[i])
+		{
+			l = 1;
+		}
+		else
+		{
+			while (A[i - 1] == A[i]) {
+				l++;
+				i++;
+			};
+			k++;
+		};
+		h = A[i];
+		if (l <= L) {
+			p++;
+		};
+	};
+	int* B = new int[k + p];
+	h = A[0];
+	l = 1;
+	for (i = 0; i < (p + k); i++) {
+		if (h != A[j])
+		{
+			B[i] = h;
+		}
+		else
+		{
+			g = j - 1;
+			while (A[j - 1] == A[j]) {
+				j++;
+				l++;
+			};
+			if (l > L) {
+				B[i] = 0;
+			}
+			else if (l <= L) {
+				while ((g) <= (j)) {
+					B[i] = A[g];
+					i++;
+					g++;
+				};
+			};
+		};
+		l = 1;
+		h = A[j];
+		j++;
+	};
+	cout << "Измененный массив: " << endl;
+	for (i = 0; i < p + k; i++) {
+		cout << B[i] << endl;
+	};
+}
---
+++ Laba_17_3.cpp
@@ -0,0 +1,70 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N, i, h, l, k, j, p, K, g, gp, pp;
+	k = 0;
+	p = 0;
+	j = 1;
+	g = 0;
+	cout << "Введите размер массива: ";
+	cin >> N;
+	int* A = new int[N];	
+	int* B = new int[N];
+	cout << "Введите элементы массива: " << endl;
+	for (i = 0; i < N; i++) {
+		cin >> A[i];
+	};
+	cout << "Введите номер серии К: ";
+	cin >> K;
+	h = A[0];
+	for (i = 1; i <= N; i++) {
+		if (h != A[i])
+		{
+			k++;
+		}
+		else
+		{
+			l = i - 1;
+			while (A[i - 1] == A[i]) {
+				i++;
+				j++;
+			};
+			k++;
+		};
+		if (k == K) {
+			g = l;
+			p = j;
+		};
+		h = A[i];
+	};
+	gp = l;
+	pp = j;
+	h = 1;
+	while (i < g) {
+		B[i] = A[i];
+		i++;
+	};
+	while (h <= pp) {
+		B[i] = A[gp];
+		gp++;
+		i++;
+		h++;
+	};
+	while (i < gp) {
+		B[i] = A[i + pp];
+		i++;
+	};
+	while (i < N) {
+		B[i] = A[g];
+		i++;
+		g++;
+	};
+	cout << "Измененный массив: " << endl;
+	for (i = 0; i < N; i++) {
+		cout << B[i] << endl;
+	};
+}
